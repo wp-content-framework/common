add0d41772f6aa87ec056dd65ef68212406bae2b
technote <technote.space@gmail.com>
2019-01-27 22:10:58 +0900

    trivial change

--- src/classes/models/define.php
+++ src/classes/models/define.php
@@ -76,7 +76,7 @@ class Define implements \WP_Framework_Core\Interfaces\Singleton {
 		$this->plugin_name = $this->app->plugin_name;
 		$this->plugin_file = $this->app->plugin_file;
 
-		$this->plugin_namespace = ucwords( $this->plugin_name, '_' );
+		$this->plugin_namespace = ucwords( strtolower( $this->plugin_name ), '_' );
 		$this->plugin_dir       = dirname( $this->plugin_file );
 		$this->plugin_dir_name  = basename( $this->plugin_dir );
 		$this->plugin_base_name = $this->app->is_theme ? 'theme/' . $this->plugin_dir : plugin_basename( $this->plugin_file );
