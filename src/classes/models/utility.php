4319cea826ca67aff037c10ac600d515e7d9dced
technote <technote.space@gmail.com>
2019-03-04 23:07:18 +0900

    add: can use block editor

--- src/classes/models/utility.php
+++ src/classes/models/utility.php
@@ -508,7 +508,16 @@ class Utility implements \WP_Framework_Core\Interfaces\Singleton {
 	public function is_valid_tinymce_color_picker() {
 		global $wp_version;
 
-		return version_compare( $wp_version, '4.0', '>=' );
+		return version_compare( $wp_version, '4.0.0', '>=' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function can_use_block_editor() {
+		global $wp_version;
+
+		return version_compare( $wp_version, '5.0.0', '>=' );
 	}
 
 	/**
