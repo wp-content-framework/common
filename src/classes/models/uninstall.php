c8f1f3f71abe9cddaff3eca554293685b2516430
Technote <technote.space@gmail.com>
2019-03-24 14:35:37 +0900

    v0.0.38

--- src/classes/models/uninstall.php
+++ src/classes/models/uninstall.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Common Classes Models Uninstall
  *
- * @version 0.0.31
+ * @version 0.0.38
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
