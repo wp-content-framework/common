501cb1198c2a9a77dbcc8345b88c74fdde7ceded
Technote <technote.space@gmail.com>
2019-07-01 03:04:40 +0900

    v0.0.52

--- src/classes/models/file_utility.php
+++ src/classes/models/file_utility.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Common Classes Models File Utility
  *
- * @version 0.0.49
+ * @version 0.0.52
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
